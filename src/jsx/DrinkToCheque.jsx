c2d3f8763e4e5d88b4bf8b10724b9eae72304915
Xialess <zzenplay@gmail.com>
2023-12-07 13:10:13 +0300

    форматирование

--- src/jsx/DrinkToCheque.jsx
+++ src/jsx/DrinkToCheque.jsx
@@ -8,6 +8,13 @@ const DrinkToCheque = (props) => {
     const [syrupValue, setSyrupValue] = useState(0);
     const [extraForDrinkValue, setExtraForDrinkValue] = useState(0);
 
+    const [currentContent, setCurrentContent] = useState("drinkValue");
+
+    const handleChangeContent = (drinkValue) => {
+        setChoisedValue(drinkValue);
+        setCurrentContent("extrasForDrink");
+    }
+
     const incrementSyrup = () => {
         if (syrupValue < 10) {
             setSyrupValue(syrupValue + 1);
@@ -36,52 +43,65 @@ const DrinkToCheque = (props) => {
         <>
             <div className="cheque-container">
                 <div className="cheque-drink-name">{choisedValue} {props.selectedDrink}</div>
-                {/* Здесь вы можете добавить содержимое своего DrinkToCheque компонента */}
             </div>
-            <div className="drink-to-cheque-buttons-container value-buttons">
-                <div onClick={() => setChoisedValue("Большой")}
-                    className="drink-to-cheque-button">
-                    Большой
-                </div>
-                <div onClick={() => setChoisedValue("Стандартный")}
-                    className="drink-to-cheque-button">
-                    Стандартный
-                </div>
-            </div>
-            <div className="drink-to-cheque-extra-for-drink-container">
-                <div className="drink-to-cheque-extra-for-drink">
-                    <div className="drink-to-cheque-extra-for-drink-title">
-                        Сироп
+            {currentContent === "drinkValue" ? (
+                <div className="drink-to-cheque-buttons-container value-buttons">
+                    <div onClick={() => handleChangeContent("Большой")}
+                        className="drink-to-cheque-button">
+                        Большой
                     </div>
-                    <div className="drink-to-cheque-extra-for-drink-buttons">
-                        <button onClick={incrementSyrup} className="drink-to-cheque-extra-for-drink-button">
-                            +
-                        </button>
-                        <div className="drink-to-cheque-extra-for-drink-value">
-                            {syrupValue}
+                    <div onClick={() => handleChangeContent("Стандартный")}
+                        className="drink-to-cheque-button">
+                        Стандартный
+                    </div>
+                </div>
+            ) : (
+                <div className="dtink-to-cheque-extra-for-drink-wrapper">
+                    <div className="drink-to-cheque-extra-for-drink-container">
+                        <div className="drink-to-cheque-extra-for-drink">
+                            <div className="drink-to-cheque-extra-for-drink-title">
+                                Сироп
+                            </div>
+                            <div className="drink-to-cheque-extra-for-drink-buttons">
+                                <button onClick={incrementSyrup} className="drink-to-cheque-extra-for-drink-button">
+                                    +
+                                </button>
+                                <div className="drink-to-cheque-extra-for-drink-value">
+                                    {syrupValue}
+                                </div>
+                                <button onClick={decrementSyrup} className="drink-to-cheque-extra-for-drink-button">
+                                    -
+                                </button>
+                            </div>
                         </div>
-                        <button onClick={decrementSyrup} className="drink-to-cheque-extra-for-drink-button">
-                            -
-                        </button>
-                    </div>
-                </div>
-                <div className="drink-to-cheque-extra-for-drink">
-                    <div className="drink-to-cheque-extra-for-drink-title">
-                        Добавки
-                    </div>
-                    <div className="drink-to-cheque-extra-for-drink-buttons">
-                        <button onClick={incrementExtras} className="drink-to-cheque-extra-for-drink-button">
-                            +
-                        </button>
-                        <div className="drink-to-cheque-extra-for-drink-value">
-                            {extraForDrinkValue}
+                        <div className="drink-to-cheque-extra-for-drink">
+                            <div className="drink-to-cheque-extra-for-drink-title">
+                                Добавки
+                            </div>
+                            <div className="drink-to-cheque-extra-for-drink-buttons">
+                                <button onClick={incrementExtras} className="drink-to-cheque-extra-for-drink-button">
+                                    +
+                                </button>
+                                <div className="drink-to-cheque-extra-for-drink-value">
+                                    {extraForDrinkValue}
+                                </div>
+                                <button onClick={decrementExtras} className="drink-to-cheque-extra-for-drink-button">
+                                    -
+                                </button>
+                            </div>
                         </div>
-                        <button onClick={decrementExtras} className="drink-to-cheque-extra-for-drink-button">
-                            -
+                    </div>
+                    <div className="drink-to-cheque-extra-for-drink-navigation-buttons">
+                        <button onClick={() => setCurrentContent("drinkValue")} className="drink-to-cheque-extra-for-drink-navigation-button">
+                            Назад
+                        </button>
+                        <button className="drink-to-cheque-extra-for-drink-navigation-button">
+                            Готово
                         </button>
                     </div>
                 </div>
-            </div>
+            )}
+
             <button onClick={() => props.switchPanel("drinks")}>Вернуться</button>
         </>
     );
