43d99aa84f7ab397e9180198a3f513aac3382e77
kandlash <zzenplay@gmail.com>
2024-03-14 12:01:21 +0300

    нужно

--- src/jsx/WorkPage.jsx
+++ src/jsx/WorkPage.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import TimePanel from "./TimePanel";
 import DrinksPanel from "./DrinksPanel";
 
 const WorkPage = () =>{
+    const [drinkToCheque, setDrinkToCheque] = useState("close");
+
+    const handleDrinkCardClick = (drinkName) =>{
+        setDrinkToCheque(open);
+    }
+
     return(
         <>
             <TimePanel></TimePanel>
