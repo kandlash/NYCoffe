5948104c66c934b9a2a4dbd1fdf6c8763c780af9
kandlash <zzenplay@gmail.com>
2024-04-02 23:12:03 +0300

    Страница активации готовые стили

--- src/jsx/ActivationPage.jsx
+++ src/jsx/ActivationPage.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/activation_page.css"
+import key_icon from "..//images/key_icon.svg"
 
-const ActivationPage = ({onButtonClick}) => {
+const ActivationPage = ({ onButtonClick }) => {
 
-    const handleClick = () =>{
-        onButtonClick();
-    }
+    // const handleClick = () => {
+    //     onButtonClick();
+    // }
 
     return (
         <div className="activation-work-wrapper">
             <div className="activation-panel">
-                <div className="auth-current-panel-title-container">
-                    <div className="auth-current-panel-title">
-                        NYCOFFEE
+                <div className="activation-panel-title">
+                    NYCOFFEE
+                </div>
+                <div className="activation-input-container">
+                    <img alt="key_icon" className="activation-input-key-icon" src={key_icon}/>
+                    <div className="activation-input">
+                        <input type="password" placeholder="Введите ключ активации" />
                     </div>
                 </div>
-                    <div className="activation-wrapper">
-                        <div className="activation-input">
-                            <input type="password" placeholder="Введите ключ активации" />
-                        </div>
-                        <div className="activation-button-containet">
-                            <Link to="/authtowork">
-                                <button className="activation-button">Активировать</button>
-                            </Link>
-                        </div>
+                <div className="activation-activate-or-skip-container">
+                    <div className="activation-button-container">
+                        <Link to="/authtowork">
+                            <button className="activation-button">Активировать</button>
+                        </Link>
+                    </div>
+                    <div className="activation-skip">
                         <Link to="/auth" className="without-activation-link">
                             Продолжить без активации
                         </Link>
                     </div>
 
+                </div>
             </div>
         </div>
     )
