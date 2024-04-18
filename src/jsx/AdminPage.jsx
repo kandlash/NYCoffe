6e7dbaec21a294fe3a65855b81ddbb61e8b396fc
kandlash <zzenplay@gmail.com>
2024-04-18 10:11:00 +0300

    Рабочий выбор сотрудников

--- src/jsx/AdminPage.jsx
+++ src/jsx/AdminPage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import ProfileLeft from "./ProfileLeft";
 import WorkNavigation from "./WorkNavigation";
 import AdminShifts from "./AdminShifts";
 import "../css/admin_page.css"
+import AdminSheduleFilling from "./AdminSheduleFilling";
 
 const AdminPage = () => {
+    const [notifications, setNotifications] = useState([]);
+
+    const handleSaveNotification = (data) => {
+        // Добавляем новое уведомление в начало списка
+        setNotifications([data, ...notifications]);
+        console.log("set notification " + data.text)
+    };
     return(
         <div className="admin-page-wrapper">
             <WorkNavigation border="noborder" name="Главная" isAdmin={true}></WorkNavigation>
             <div className="admin-page-elements-container">                
-                <ProfileLeft name="Анатольев Анатолий Анатольевич" position="Админ"></ProfileLeft>
-                <AdminShifts></AdminShifts>
+                <ProfileLeft name="Анатольев Анатолий Анатольевич" position="Админ" notification={notifications[0]} previousNotifications={notifications.slice(1)}></ProfileLeft>
+                <AdminSheduleFilling onSave={handleSaveNotification}/>
             </div>
         </div>
     )
--- src/jsx/AdminSheduleFilling.jsx
+++ src/jsx/AdminSheduleFilling.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import moment from "moment";
 import left_arrow from "../images/left_arrow.svg";
 import right_arrow from "../images/right_arrow.svg";
 
-const AdminSheduleFilling = () => {
-    // Состояние для хранения выбранных смен
+const AdminSheduleFilling = ({ onSave }) => {
+    // State for storing selected shifts
     const [selectedShifts, setSelectedShifts] = useState({
         Monday: [[], [], []],
         Tuesday: [[], [], []],
@@ -12,142 +12,171 @@ const AdminSheduleFilling = () => {
         Thursday: [[], [], []],
         Friday: [[], [], []],
         Saturday: [[], [], []],
-        Sunday: [[], [], []]
+        Sunday: [[], [], []],
     });
 
-    // Состояние для хранения текущей отображаемой недели
-    const [currentWeek, setCurrentWeek] = useState(moment().startOf('isoWeek'));
+    const [backData, setBackData] = useState([]);
 
-    // Состояние для списка доступных сотрудников
-    const [availableEmployees, setAvailableEmployees] = useState([]);
+    // State for storing the current displayed week
+    const [currentWeek, setCurrentWeek] = useState(moment().startOf("isoWeek"));
 
-    // Функция для переключения на предыдущую неделю
+    // Function to go to the previous week
     const goToPreviousWeek = () => {
-        setSelectedShifts({}); // Очищаем выбранные смены
-        setCurrentWeek(prevWeek => prevWeek.clone().subtract(1, 'week'));
+        setSelectedShifts({}); // Clear selected shifts
+        setCurrentWeek((prevWeek) => prevWeek.clone().subtract(1, "week"));
     };
 
-    // Функция для переключения на следующую неделю
+    // Function to go to the next week
     const goToNextWeek = () => {
-        setSelectedShifts({}); // Очищаем выбранные смены
-        setCurrentWeek(prevWeek => prevWeek.clone().add(1, 'week'));
+        setSelectedShifts({}); // Clear selected shifts
+        setCurrentWeek((prevWeek) => prevWeek.clone().add(1, "week"));
     };
 
-    // Функция для форматирования текущей недели
-    const formatCurrentWeek = () => {
-        const startOfWeek = currentWeek.startOf('isoWeek').format('DD.MM.YYYY');
-        const endOfWeek = currentWeek.endOf('isoWeek').format('DD.MM.YYYY');
+    // Function to format the current week
+    const formatCurrentWeek = useCallback(() => {
+        const startOfWeek = currentWeek.startOf("isoWeek").format("DD.MM.YYYY");
+        const endOfWeek = currentWeek.endOf("isoWeek").format("DD.MM.YYYY");
         return `${startOfWeek}-${endOfWeek}`;
-    };
+    }, [currentWeek]);
 
-    // Получение данных с бэкэнда
-    useEffect(() => {
-        // Здесь ваш код для получения данных с бэкэнда
-        const backendData = [
-            {employe_name: "Кириллов Кирилл Кириллович", day: "Monday", shift:1},
-            {employe_name: "Чурилов Чурил Чурилович", day: "Tuesday", shift:2},
-            {employe_name: "Кириллов Кирилл Кириллович", day: "Tuesday", shift:3},
-            {employe_name: "Жуков Жук Жукович", day: "Tuesday", shift:2},
-            {employe_name: "Петров Петр Петрович", day: "Friday", shift:1}
-        ];
+    // Function to fetch data from the backend
+    const fetchData = useCallback(() => {
+        const cw = formatCurrentWeek();
+        let backendData = [];
 
-        // Обновление списка доступных сотрудников
-        const employees = backendData.map(data => data.employe_name);
-        setAvailableEmployees(employees);
+        if (cw === "15.04.2024-21.04.2024") {
+            backendData = [
+                { employe_name: "Кириллов Кирилл Кириллович", day: "Monday", shift: 1 },
+                { employe_name: "Чурилов Чурил Чурилович", day: "Tuesday", shift: 2 },
+                { employe_name: "Кириллов Кирилл Кириллович", day: "Tuesday", shift: 3 },
+                { employe_name: "Жуков Жук Жукович", day: "Tuesday", shift: 2 },
+                { employe_name: "Петров Петр Петрович", day: "Friday", shift: 1 },
+            ];
+        } else if (cw === "08.04.2024-14.04.2024") {
+            backendData = [
+                { employe_name: "Кириллов Кирилл Кириллович", day: "Monday", shift: 3 },
+                { employe_name: "Чурилов Чурил Чурилович", day: "Tuesday", shift: 3 },
+                { employe_name: "Кириллов Кирилл Кириллович", day: "Tuesday", shift: 3 },
+                { employe_name: "Жуков Жук Жукович", day: "Tuesday", shift: 1 },
+                { employe_name: "Петров Петр Петрович", day: "Friday", shift: 1 },
+            ];
+        } else if (cw === "22.04.2024-28.04.2024") {
+            backendData = [
+                { employe_name: "Кириллов Кирилл Кириллович", day: "Friday", shift: 2 },
+                { employe_name: "Чурилов Чурил Чурилович", day: "Monday", shift: 1 },
+                { employe_name: "Кириллов Кирилл Кириллович", day: "Tuesday", shift: 3 },
+                { employe_name: "Жуков Жук Жукович", day: "Tuesday", shift: 2 },
+                { employe_name: "Петров Петр Петрович", day: "Friday", shift: 1 },
+            ];
+        }
+        console.log("fetched data: " + backendData);
+        // Update the state with fetched data
+        setSelectedShifts(initializeSelectedShifts(backendData));
+        setBackData(backendData);
+    }, [formatCurrentWeek]);
 
-        // Обновление состояния selectedShifts
-        const updatedSelectedShifts = {
+    // Function to initialize selectedShifts state
+    const initializeSelectedShifts = (data) => {
+        const selectedShifts = {
             Monday: [[], [], []],
             Tuesday: [[], [], []],
             Wednesday: [[], [], []],
             Thursday: [[], [], []],
             Friday: [[], [], []],
             Saturday: [[], [], []],
-            Sunday: [[], [], []]
+            Sunday: [[], [], []],
         };
 
-        backendData.forEach(data => {
-            const { employe_name, day, shift } = data;
+        data.forEach(({ employe_name, day, shift }) => {
             const index = shift - 1;
-            if (employees.includes(employe_name)) {
-                if (!updatedSelectedShifts[day][index].includes(employe_name)) {
-                    updatedSelectedShifts[day][index].push(employe_name);
-                }
-            }
+            selectedShifts[day][index].push(employe_name);
         });
 
-        setSelectedShifts(updatedSelectedShifts);
-    }, []);
-
-    // Функция для обновления выбранной смены
-    const handleShiftChange = (day, index, employee) => {
-        setSelectedShifts(prevState => {
-            const newState = { ...prevState };
-            newState[day][index] = [employee];
-            return newState;
-        });
+        return selectedShifts;
     };
 
-    // Функция для отправки данных на бэкэнд
+    // Function to send data to the backend
     const handleSendData = () => {
         const dataToSend = {
-            week: formatCurrentWeek(), // Форматируем текущую неделю
-            shifts: selectedShifts    // Выбранные смены
+            week: formatCurrentWeek(), // Format current week
+            shifts: selectedShifts, // Selected shifts
         };
         console.log(dataToSend);
-    
-        //sendDataToBackend(dataToSend); // Отправляем данные на сервер
+        const data = {
+            text: "Заполнен график на неделю: ",
+            date: formatCurrentWeek(),
+        };
+        onSave(data); // Call the callback and pass the data
+        //sendDataToBackend(dataToSend); // Send data to server
     };
 
-    return(
-        <div className="schedule-filling-container">
-            <div className="shedule-filling-table">
-                <table>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Пн</th>
-                            <th>Вт</th>
-                            <th>Ср</th>
-                            <th>Чт</th>
-                            <th>Пт</th>
-                            <th>Сб</th>
-                            <th>Вс</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {[1, 2, 3].map(shift => (
-                            <tr key={shift}>
-                                <td>{shift} смена</td>
-                                {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => (
-                                    <td key={`${day}-${shift}`}>
-                                        <select value={selectedShifts[day][shift-1][0] || ""} onChange={(e) => handleShiftChange(day, shift-1, e.target.value)} style={{backgroundColor: selectedShifts[day][shift-1][0] ? 'green' : 'white'}}>
-                                            <option value="">Выберите сотрудника</option>
-                                            {availableEmployees.filter(employee => selectedShifts[day][shift-1].includes(employee)).map((employee, index) => (
-                                                <option key={index} value={employee}>{employee}</option>
-                                            ))}
-                                        </select>
-                                    </td>
-                                ))}
+    // Fetch data from the backend
+    useEffect(() => {
+        console.log("обновили страницу!");
+        fetchData();
+    }, [fetchData]);
+
+    useEffect(() => {
+        console.log("изменили неделю!");
+        fetchData();
+    }, [currentWeek, fetchData]);
+
+    return (
+        <div className="admin-shifts-wrapper">
+            <div className="schedule-filling-container">
+                <div className="shedule-filling-table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Пн</th>
+                                <th>Вт</th>
+                                <th>Ср</th>
+                                <th>Чт</th>
+                                <th>Пт</th>
+                                <th>Сб</th>
+                                <th>Вс</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className="shedule-filling-week-swap">
-                <button onClick={goToPreviousWeek}>
-                    <img src={left_arrow} alt="leftarrow"/>
-                </button>
-                <p>{formatCurrentWeek()}</p>
-                <button onClick={goToNextWeek}>
-                    <img src={right_arrow} alt="rightarrow"/>
-                </button>
-            </div>
-            <div className="shedule-filling-week-save">
-                <button onClick={handleSendData}>Сохранить</button>
+                        </thead>
+                        <tbody>
+                            {[1, 2, 3].map((shift) => (
+                                <tr key={shift}>
+                                    <td>{shift} смена</td>
+                                    {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map((day) => (
+                                        <td key={`${day}-${shift}`}>
+                                            <select onClick={() => console.log(day + " " + shift)}>
+                                                {/* <option>{backData.length}</option> */}
+                                                {backData && backData.map((employee, index) => {
+                                                    if (employee.day === day && employee.shift === shift) {
+                                                        return <option key={`${employee.id}-${day}-${shift}-${index}`}>{employee.employe_name}</option>;
+                                                    }
+                                                    return null;
+                                                })}
+
+                                            </select>
+                                        </td>
+
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="shedule-filling-week-swap">
+                    <button onClick={goToPreviousWeek}>
+                        <img src={left_arrow} alt="leftarrow" />
+                    </button>
+                    <p>{formatCurrentWeek()}</p>
+                    <button onClick={goToNextWeek}>
+                        <img src={right_arrow} alt="rightarrow" />
+                    </button>
+                </div>
+                <div className="shedule-filling-week-save">
+                    <button onClick={handleSendData}>Сохранить</button>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AdminSheduleFilling;
