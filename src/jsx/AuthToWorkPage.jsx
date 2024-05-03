84e49502624d158f6bc1c98ed8ce8883448dc3e7
kandlash <zzenplay@gmail.com>
2024-05-03 14:14:54 +0300

    Вход в смену

--- src/jsx/AuthToWorkPage.jsx
+++ src/jsx/AuthToWorkPage.jsx
@@ -4,10 +4,14 @@ import '../css/authpanel.css'
 import AuthContext from "./AuthContext";
 
 const AuthToWorkPage = () => {
-
-    const {loginAsWorker, deActivate, isActivated, isAdmin, isWorker, isEmploye} = useContext(AuthContext);
+    const { loginAsWorker, deActivate, isActivated, isAdmin, isWorker, isEmploye, employees } = useContext(AuthContext);
 
     const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [isCorrectInput, setIsCorrectInput] = useState(true);
 
     useEffect(() => {
         if (isAdmin) {
@@ -16,11 +20,40 @@ const AuthToWorkPage = () => {
             navigate("/work");
         } else if (isEmploye) {
             navigate("/profile");
-        }
-        else if(!isActivated){
+        } else if (!isActivated) {
             navigate("/");
         }
     }, [isActivated, isAdmin, isWorker, isEmploye, navigate]);
+
+    useEffect(() => {
+        if (isCorrect) {
+            navigate("/work");
+        }
+    }, [isCorrect, navigate]);
+
+    const handleLoginAsWorker = () => {
+        const employee = employees.find(
+            (employee) => employee.email === email && employee.password === password
+        );
+
+        if (employee) {
+            loginAsWorker();
+            setIsCorrect(true);
+            setIsCorrectInput(true);
+        } else {
+            console.log("not correct!");
+            setIsCorrectInput(false);
+            // здесь вы можете добавить логику для отображения сообщения об ошибке
+        }
+    }
+
+    const handleEmailChange = () =>{
+        setIsCorrectInput(true);
+    }
+
+    const handlePasswordChange = () =>{
+        setIsCorrectInput(true);
+    }
 
     return (
         <div className="AuthPanelWrapper">
@@ -30,21 +63,23 @@ const AuthToWorkPage = () => {
                         Авторизация
                     </div>
                 </div>
-                    <div className="auth-wrapper">
-                        <div className="auth-input">
-                            <input className="auth-input-in" type="text" placeholder="Номер телефона или Email" />
-                        </div>
-                        <div className="auth-input">
-                            <input className="auth-input-in" type="password" placeholder="Пароль" />
-                        </div>
-                        <div className="auth-button-container-">
-                            <Link onClick={loginAsWorker} to="/work">
-                                <button className="login-button">Начать смену</button>
-                            </Link>
-                            <button onClick={deActivate} className="login-button">Деактивировать</button>
-                        </div>
+                <div className="auth-wrapper">
+                    <div className="auth-input">
+                        <input className={`auth-input-in ${isCorrectInput ? '' : 'not-correct-input'}`} value={email} onChange={(e) => { setEmail(e.target.value); handleEmailChange(); }} type="text" placeholder="Номер телефона или Email" />
                     </div>
-
+                    <div className="auth-input">
+                        <input className={`auth-input-in ${isCorrectInput ? '' : 'not-correct-input'}`} value={password} onChange={(e) => { setPassword(e.target.value); handlePasswordChange(); }} type="password" placeholder="Пароль" />
+                    </div>
+                    {!isCorrectInput &&
+                        <div className="not-correct-auth-text">
+                            Не верный логин или пароль. Попробуйте еще раз.
+                        </div>
+                    }
+                    <div className="auth-button-container-">
+                        <button onClick={handleLoginAsWorker} className="login-button">Начать смену</button>
+                        <button onClick={deActivate} className="login-button">Деактивировать</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
